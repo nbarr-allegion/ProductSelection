f5a12f9da0e080b089d5495d68dbf8e755ef031a
nathan barr <nbarr013@gmail.com>
2019-05-01 10:45:40 -0400

    now uses notifications

--- poshShareCloset.js
+++ poshShareCloset.js
@@ -1,4 +1,5 @@
 let shares = [];
+let triggerNotification = false;
 $("div.col-x12.col-l6.col-s8 .tile").each(function () {
     let icon = $(this).find(".covershot-con i");
     if (icon.length === 0) {
@@ -10,11 +11,6 @@ function clickShareButton() {
     for (let i = 0; i < shares.length; i++) {
         setTimeout(function () {
             if (shares[i] != null && shares[i] !== "") {
-                if($("#captcha-popup").length != 0){
-                    if($("#captcha-popup").hasClass("in")){
-                        $(".recaptcha-checkbox-checkmark").click();
-                    }
-                }
                 $("a.pm-followers-share-link").attr("href", "/listing/share?post_id=" + shares[i]);
                 $("a.pm-followers-share-link").click();
 
@@ -28,3 +24,48 @@ for (let i = 0; i < 3000; i++) {
         clickShareButton();
     }, i * shares.length * 5000)
 }
+
+
+function clickcaptcha() {
+    if($("#captcha-popup").length != 0){
+        if($("#captcha-popup").hasClass("in")){
+            $(".recaptcha-checkbox-checkmark").click();
+        }
+    }
+    $(".recaptcha-checkbox-checkmark").click();
+    document.querySelector(".recaptcha-checkbox-checkmark").click();
+}
+
+function dealWithCaptcha() {
+    if(!triggerNotification){
+        if (Notification.permission !== "granted")
+            Notification.requestPermission();
+        else {
+            let notification = new Notification('Captcha Has Appeared!', {
+                icon: 'https://d2zlsagv0ouax1.cloudfront.net/assets/v3/logo@2x-6003c7f00d83f4df697830d18bdcf167.png',
+                body: "Go to posh and deal with it",
+                silent: false,
+                vibrate: true,
+            });
+            notification.onshow = function () {
+                let audio = new Audio('http://soundbible.com/mp3/fire-truck-air-horn_daniel-simion.mp3');
+                audio.volume = 1;
+                audio.play();
+            }
+        }
+        triggerNotification = true;
+        setTimeout(function () {
+            triggerNotification = false;
+        }, 180000)
+    }
+    clickcaptcha();
+
+}
+
+window.onerror = function (errorMsg, url, lineNumber, column, errorObj) {
+    dealWithCaptcha();
+
+};
+$(document).ajaxError(function( event, request, settings ) {
+    dealWithCaptcha();
+});
--- poshShareFeed.js
+++ poshShareFeed.js
@@ -1,4 +1,6 @@
 let shares = [];
+let triggerNotification = false;
+
 $("div.col-x12.col-l6.col-s8 .tile").each(function () {
     let icon = $(this).find(".covershot-con i")
     if (icon.length === 0) {
@@ -8,11 +10,7 @@ $("div.col-x12.col-l6.col-s8 .tile").each(function () {
 for (let i = 0; i < shares.length; i++) {
     setTimeout(function () {
         if (shares[i] != null && shares[i] !== "") {
-            if($("#captcha-popup").length != 0){
-                if($("#captcha-popup").hasClass("in")){
-                    $(".recaptcha-checkbox-checkmark").click();
-                }
-            }
+
             $("a.pm-followers-share-link").attr("href", "/listing/share?post_id=" + shares[i]);
             $("a.pm-followers-share-link").click();
         }
@@ -24,3 +22,49 @@ setTimeout(function () {
 }, shares.length * 2000);
 let time = (shares.length * 2) / 60;
 console.log("Number of items to share: " + shares.length + "\nTime to completion: " + time + " minutes.");
+
+
+
+
+function clickcaptcha() {
+    if($("#captcha-popup").length != 0){
+        if($("#captcha-popup").hasClass("in")){
+            $(".recaptcha-checkbox-checkmark").click();
+        }
+    }
+    $(".recaptcha-checkbox-checkmark").click();
+    document.querySelector(".recaptcha-checkbox-checkmark").click();
+}
+
+function dealWithCaptcha() {
+    if(!triggerNotification){
+        if (Notification.permission !== "granted")
+            Notification.requestPermission();
+        else {
+            let notification = new Notification('Captcha Has Appeared!', {
+                icon: 'https://d2zlsagv0ouax1.cloudfront.net/assets/v3/logo@2x-6003c7f00d83f4df697830d18bdcf167.png',
+                body: "Go to posh and deal with it",
+                silent: false,
+                vibrate: true,
+            });
+            notification.onshow = function () {
+                let audio = new Audio('http://soundbible.com/mp3/fire-truck-air-horn_daniel-simion.mp3');
+                audio.volume = 1;
+                audio.play();
+            }
+        }
+        triggerNotification = true;
+        setTimeout(function () {
+            triggerNotification = false;
+        }, 180000)
+    }
+    clickcaptcha();
+}
+
+window.onerror = function (errorMsg, url, lineNumber, column, errorObj) {
+    dealWithCaptcha();
+
+};
+$(document).ajaxError(function( event, request, settings ) {
+    dealWithCaptcha();
+});
