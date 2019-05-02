d26bd3b4d065dd8999cf1223933f6da01e8c3b89
nathan barr <nbarr013@gmail.com>
2019-05-02 16:08:35 -0400

    bookmarklet

--- poshShareCloset.js
+++ poshShareCloset.js
@@ -27,8 +27,8 @@ for (let i = 0; i < 3000; i++) {
 
 
 function clickcaptcha() {
-    if($("#captcha-popup").length != 0){
-        if($("#captcha-popup").hasClass("in")){
+    if ($("#captcha-popup").length != 0) {
+        if ($("#captcha-popup").hasClass("in")) {
             $(".recaptcha-checkbox-checkmark").click();
         }
     }
@@ -37,7 +37,7 @@ function clickcaptcha() {
 }
 
 function dealWithCaptcha() {
-    if(!triggerNotification){
+    if (!triggerNotification) {
         if (Notification.permission !== "granted")
             Notification.requestPermission();
         else {
@@ -48,7 +48,7 @@ function dealWithCaptcha() {
                 vibrate: true,
             });
             notification.onshow = function () {
-                let audio = new Audio('http://soundbible.com/mp3/fire-truck-air-horn_daniel-simion.mp3');
+                let audio = new Audio('http://soundbible.com/mp3/Bobwhite Quail-SoundBible.com-1782515847.mp3');
                 audio.volume = 1;
                 audio.play();
             }
@@ -66,6 +66,9 @@ window.onerror = function (errorMsg, url, lineNumber, column, errorObj) {
     dealWithCaptcha();
 
 };
-$(document).ajaxError(function( event, request, settings ) {
+$(document).ajaxError(function (event, request, settings) {
     dealWithCaptcha();
 });
+/*
+javascript:(function(){let shares=[],triggerNotification=!1;function clickShareButton(){for(let t=0;t<shares.length;t++)setTimeout(function(){null!=shares[t]&&""!==shares[t]&&($("a.pm-followers-share-link").attr("href","/listing/share?post_id="+shares[t]),$("a.pm-followers-share-link").click())},5e3*t)}$("div.col-x12.col-l6.col-s8 .tile").each(function(){0===$(this).find(".covershot-con i").length&&shares.push($(this).attr("id"))});for(let t=0;t<3e3;t++)setTimeout(function(){clickShareButton()},t*shares.length*5e3);function clickcaptcha(){0!=$("#captcha-popup").length&&$("#captcha-popup").hasClass("in")&&$(".recaptcha-checkbox-checkmark").click(),$(".recaptcha-checkbox-checkmark").click(),document.querySelector(".recaptcha-checkbox-checkmark").click()}function dealWithCaptcha(){if(!triggerNotification){if("granted"!==Notification.permission)Notification.requestPermission();else{new Notification("Captcha Has Appeared!",{icon:"https://d2zlsagv0ouax1.cloudfront.net/assets/v3/logo@2x-6003c7f00d83f4df697830d18bdcf167.png",body:"Go to posh and deal with it",silent:!1,vibrate:!0}).onshow=function(){let t=new Audio("http://soundbible.com/mp3/Bobwhite Quail-SoundBible.com-1782515847.mp3");t.volume=1,t.play()}}triggerNotification=!0,setTimeout(function(){triggerNotification=!1},18e4)}clickcaptcha()}window.onerror=function(t,i,c,e,o){dealWithCaptcha()},$(document).ajaxError(function(t,i,c){dealWithCaptcha()});})();
+*/
--- poshShareFeed.js
+++ poshShareFeed.js
@@ -48,7 +48,7 @@ function dealWithCaptcha() {
                 vibrate: true,
             });
             notification.onshow = function () {
-                let audio = new Audio('http://soundbible.com/mp3/fire-truck-air-horn_daniel-simion.mp3');
+                let audio = new Audio('http://soundbible.com/mp3/Bobwhite Quail-SoundBible.com-1782515847.mp3');
                 audio.volume = 1;
                 audio.play();
             }
@@ -68,3 +68,8 @@ window.onerror = function (errorMsg, url, lineNumber, column, errorObj) {
 $(document).ajaxError(function( event, request, settings ) {
     dealWithCaptcha();
 });
+
+
+/*
+javascript:(function(){let shares=[],triggerNotification=!1;$("div.col-x12.col-l6.col-s8 .tile").each(function(){0===$(this).find(".covershot-con i").length&&shares.push($(this).attr("id"))});for(let e=0;e<shares.length;e++)setTimeout(function(){null!=shares[e]&&""!==shares[e]&&($("a.pm-followers-share-link").attr("href","/listing/share?post_id="+shares[e]),$("a.pm-followers-share-link").click())},2e3*e);setTimeout(function(){new Audio("https://interactive-examples.mdn.mozilla.net/media/examples/t-rex-roar.mp3").play()},2e3*shares.length);let time=2*shares.length/60;function clickcaptcha(){0!=$("#captcha-popup").length&&$("#captcha-popup").hasClass("in")&&$(".recaptcha-checkbox-checkmark").click(),$(".recaptcha-checkbox-checkmark").click(),document.querySelector(".recaptcha-checkbox-checkmark").click()}function dealWithCaptcha(){if(!triggerNotification){if("granted"!==Notification.permission)Notification.requestPermission();else{new Notification("Captcha Has Appeared!",{icon:"https://d2zlsagv0ouax1.cloudfront.net/assets/v3/logo@2x-6003c7f00d83f4df697830d18bdcf167.png",body:"Go to posh and deal with it",silent:!1,vibrate:!0}).onshow=function(){let e=new Audio("http://soundbible.com/mp3/Bobwhite Quail-SoundBible.com-1782515847.mp3");e.volume=1,e.play()}}triggerNotification=!0,setTimeout(function(){triggerNotification=!1},18e4)}clickcaptcha()}console.log("Number of items to share: "+shares.length+"\nTime to completion: "+time+" minutes."),window.onerror=function(e,t,i,o,c){dealWithCaptcha()},$(document).ajaxError(function(e,t,i){dealWithCaptcha()});});
+*/
